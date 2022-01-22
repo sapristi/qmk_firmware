4cdf69daf7b23bd05e20b69f0fc5748198ee2142
Mathias Millet <megabigfoutoir@gmail.com>
2022-01-22 18:39:19 +0100

    fixes

--- keyboards/splitkb/kyria/keymaps/personal/rules.mk
+++ keyboards/splitkb/kyria/keymaps/personal/rules.mk
@@ -1,4 +1,4 @@
 OLED_DRIVER_ENABLE = no   # Enables the use of OLED displays
 ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
-COMBO_ENABLE = yes
+COMBO_ENABLE = no
